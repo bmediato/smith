fc642777d3b897174a23386be99b2f4876020ac6
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-14 18:47:31 -0300

    feat: finaliza 4

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -2,7 +2,11 @@ import connection from './connection';
 import { Order } from '../interfaces/order';
 
 export default async function getAll(): Promise<Order[]> {
-  const [orders] = await connection.execute(`SELECT *, JSON_ARRAYAGG(id) as products 
-    FROM Trybesmith.orders group by id;`);
+  const [orders] = await connection.execute(`SELECT o.id, o.user_id as userId,
+  JSON_ARRAYAGG(pr.id) as productsIds
+  FROM Trybesmith.orders as o
+  INNER JOIN Trybesmith.products as pr
+  ON o.id = pr.order_id
+  GROUP BY o.id;`);
   return orders as Order[];
 }
