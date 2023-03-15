610b925bf6f6f02a8f2ed0669d01f847d1e268f9
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-15 15:49:22 -0300

    fix: arruma message req6

--- src/middleware/validateProducts.ts
+++ src/middleware/validateProducts.ts
@@ -6,7 +6,7 @@ function validateName(name: string) {
   if (!name) return ({ status: 400, message: '"name" is required' });
   if (typeof name !== 'string') return ({ status: 422, message: '"name" must be a string' });
   if (name.length < 2) {
-    return ({ status: 422, message: '"name" length must be at least 2 characters long' });
+    return ({ status: 422, message: '"name" length must be at least 3 characters long' });
   }
   return null;
 }
