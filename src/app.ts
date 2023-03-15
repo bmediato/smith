7a531e6afddf950066ce0e95458b71a4e018ada7
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-15 15:19:14 -0300

    feat: add req 6

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,14 @@ import * as productController from './controllers/productController';
 import * as userController from './controllers/userController';
 import orderController from './controllers/orderController';
 import validateLogin from './middleware/validateLogin';
+import validateProduct from './middleware/validateProducts';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/products', productController.getAll);
-app.post('/products', productController.create);
+app.post('/products', validateProduct, productController.create);
 app.post('/users', userController.create);
 app.get('/orders', orderController);
 app.post('/login', validateLogin, userController.login);
---
+++ src/middleware/validateProducts.ts
@@ -0,0 +1,31 @@
+import { Request, NextFunction, Response } from 'express';
+
+import { NewProduct } from '../interfaces/product';
+
+function validateName(name: string) {
+  if (!name) return ({ status: 400, message: '"name" is required' });
+  if (typeof name !== 'string') return ({ status: 422, message: '"name" must be a string' });
+  if (name.length < 2) {
+    return ({ status: 422, message: '"name" length must be at least 3 characters long' });
+  }
+  return null;
+}
+
+function validateAmount(amount: string) {
+  if (!amount) return ({ status: 400, message: '"amount" is required' });
+  if (typeof amount !== 'string') return ({ status: 422, message: '"amount" must be a string' });
+  if (amount.length < 2) {
+    return ({ status: 422, message: '"amount" length must be at least 3 characters long' });
+  }
+  return null;
+}
+
+export default function validateProduct(req: Request, res: Response, next: NextFunction) {
+  const { name, amount } = req.body as NewProduct;
+ 
+  const errorName = validateName(name);
+  const errorAmount = validateAmount(amount);
+  if (errorName) return res.status(errorName.status).json({ message: errorName.message });
+  if (errorAmount) return res.status(errorAmount.status).json({ message: errorAmount.message });
+  next();
+}
